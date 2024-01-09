e06a9626877550110efc4a0ffb8c4ce1fd30cd9e
Kristaps <kbebris+git@gmail.com>
2024-01-09 15:51:47 +0200

    looking at how to use arrays

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
+    let state = [[0u8; 4]; 6];
+
     println!("Hello, world!");
+
+    state.iter().for_each(|r| {
+        r.iter().for_each(|c| {
+            print!("{:#?}", c);
+        });
+        print!("{}", "\n");
+    })
 }
