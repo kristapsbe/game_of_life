2c4eff21bac7c6cb3487a0870ed4bfe3967c09d1
Kristaps <kbebris+git@gmail.com>
2024-01-09 16:48:05 +0200

    poking at vectors

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,32 @@
-fn main() {
-    let state = [[0u8; 4]; 6];
+use std::{thread, time};
 
-    println!("Hello, world!");
 
+fn print_state(state: Vec<Vec<bool>>) {
     state.iter().for_each(|r| {
         r.iter().for_each(|c| {
-            print!("{:#?}", c);
+            if *c {
+                print!("{}", "#");
+            } else {
+                print!("{}", "_");
+            }
         });
         print!("{}", "\n");
     })
+}
+
+fn main() {
+    let w = 10;
+    let h = 10;
+    let one_second = time::Duration::from_millis(1000);
+
+    for i in 0..h {
+        for j in 0..w {
+            let mut state = vec![vec![false; w]; h];
+            state[i][j] = true;
+
+            thread::sleep(one_second);
+            print!("\x1B[2J\x1B[1;1H"); // clear the terminal and put the cursor at 1:1
+            print_state(state);
+        }
+    }
 }
